c4a1c4f4aaf0e149b628659d0bd4fec4bc10e35a
axili39 <axili39@github.com>
2020-04-19 10:09:25 +0000

    change test mode

--- test/test.sh
+++ test/test.sh
@@ -1,32 +1,12 @@
-# source : bin
-./demoTool -r A -f data.bin -o json > /dev/null
-diff data.json output.json
-[ $? -ne 0 ] && exit "bin test error"
-./demoTool -r A -f data.bin -o yaml > /dev/null
-diff data.yaml output.yaml
-[ $? -ne 0 ] && exit "bin test error"
-./demoTool -r A -f data.bin -o bin > /dev/null
-diff data.bin output.bin
-[ $? -ne 0 ] && exit "bin test error"
-
-# source json
-./demoTool -r A -f data.json -o json > /dev/null
-diff data.json output.json
-[ $? -ne 0 ] && exit "json test error"
-./demoTool -r A -f data.json -o yaml > /dev/null
-diff data.yaml output.yaml
-[ $? -ne 0 ] && exit "json test error"
-./demoTool -r A -f data.json -o bin > /dev/null
-diff data.bin output.bin
-[ $? -ne 0 ] && exit "json test error"
-
-#source yaml
-./demoTool -r A -f data.yaml -o json > /dev/null
-diff data.json output.json
-[ $? -ne 0 ] && exit "yaml test error"
-./demoTool -r A -f data.yaml -o yaml > /dev/null
-diff data.yaml output.yaml
-[ $? -ne 0 ] && exit "yaml test error"
-./demoTool -r A -f data.yaml -o bin > /dev/null
-diff data.bin output.bin
-[ $? -ne 0 ] && exit "yaml test error"
+cycletest()
+{
+	format=$1
+	./demoTool -r A -f data.yaml -o $format > /dev/null
+	./demoTool -r A -f output.$format -o yaml > /dev/null
+	diff data.yaml output.yaml
+	[ $? -ne 0 ] && exit "$format test error"
+}
+# consider reference : yaml
+cycletest json
+cycletest bin
+cycletest yaml
