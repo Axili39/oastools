5ac45b0f33c70a338474abdf68607ab932d9a5fa
axili39 <axili39@github.com>
2020-05-01 15:14:49 +0000

    add command line options to oa2proto

--- src/oa2proto/oa2proto.go
+++ src/oa2proto/oa2proto.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"github.com/Axili39/oastools/protobuf"
+	"github.com/Axili39/oastools/oasmodel"
+	"flag"
+	"os"
+	"fmt"
 )
 
 func main() {
-	protobuf.Test()
+	file := flag.String("f", "test.yaml", "yaml file to parse")
+	out := flag.String("o", "", "output file")
+	flag.Parse()
 
+	var output *os.File
+	if *out != "" {
+		var err error
+		output, err = os.Create(*out)
+		if err != nil {
+			fmt.Printf("error opening %s : %v", *out, err)
+			return
+		}
+		defer output.Close()
+	} else {
+		output = os.Stdout
+	}
+    
+	oa := oasmodel.OpenAPI{}
+	oa.Load(*file)
+	protobuf.Components2Proto(&oa, output)
 }
